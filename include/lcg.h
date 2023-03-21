cbd139532897cdb998e96166f223c2c9a1896cbd
Samiur Khan <samiurkh1n@gmail.com>
2023-03-21 17:22:29 -0400

    Add LCG::Valid() to check whether LCG values are usable

--- include/lcg.h
+++ include/lcg.h
@@ -7,13 +7,19 @@ namespace SKTL {
 namespace Random {
 namespace LCG {
 
-// TODO: assert a != 0, a < m, c < m, x < m.
 struct LinearCongruentialGenerator {
+  // TODO: perhaps each struct should carry a valid int where 0 means
+  // uninitialized, 1 means has values but unverified, 2 means initialized
+  // and verified.
   unsigned long long a;  // multiplier term
   unsigned long long c;  // increment term
   unsigned long long m;  // modulus term
   unsigned long long x;
 };
+
+bool Valid(const LinearCongruentialGenerator &lcg) {
+  return (lcg.a != 0 && lcg.a < lcg.m && lcg.c < lcg.m && lcg.x < lcg.m);
+}
 
 unsigned GenRandInt32(LinearCongruentialGenerator &lcg) {
   lcg.x = (lcg.a * lcg.x + lcg.c) % lcg.m;
--- src/lcg_test.cc
+++ src/lcg_test.cc
@@ -3,9 +3,23 @@
 #include <iostream>
 #include <random>
 
+static int kTestCount = 3;
+
 int main() {
+  int passed = 0;
+  SKTL::Random::LCG::LinearCongruentialGenerator invalid{0, 0, 0, 0};
+  if (Valid(invalid)) {
+    std::cout << "FAIL: Valid(invalid) returned true. Want false\n";
+  } else {
+    passed++;
+  }
   unsigned seed = 10217;
   auto g = SKTL::Random::LCG::InitializeMinStdRand(seed);
+  if (!Valid(g)) {
+    std::cout << "FAIL: Valid(/*valid*/g) returned false. Want true\n";
+  } else {
+    passed++;
+  }
   std::minstd_rand sg(seed);
   unsigned sktl_n, stl_n;
   for (int i = 0; i < 10000; ++i) {
@@ -13,9 +27,10 @@ int main() {
     stl_n = sg();
     if (sktl_n != stl_n) {
       std::cout << "FAIL: Got " << sktl_n << ". Want: " << stl_n << "\n";
-      return 1;
+    } else {
+      passed++;
     }
   }
-  std::cout << "PASS\n";
+  std::cout << "PASSED " << kTestCount << "/" << kTestCount << "\n";
   return 0;
 }
